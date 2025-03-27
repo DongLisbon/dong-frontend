69211e67bbabb18351af49788706c4afbad507b8
Samuel Onoja <samuelonoja0@gmail.com>
2025-03-27 03:30:57 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+      <p className="h-screen flex justify-center items-center text-center text-6xl font-bold">
+        Dong Coffee
+      </p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
